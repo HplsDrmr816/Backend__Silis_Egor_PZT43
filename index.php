d1b770a7ed9d815238f887ae76cc7cd54d7e0c96
HplsDrmr816 <egorsilis618@gmail.com>
2026-07-01 14:12:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>PHP лабы</title>
+</head>
+<body>
+    <a href = "lr1/index.php"> ЛР1</a><br>
+    <a href = "lr2/index.php"> ЛР2</a><br>
+    <a href = "lr3/index.php">ЛР3</a> <br>
+    <a href = "lr4/index.php"> ЛР4</a><br>
+    <a href = "lr5/index.php"> ЛР5</a><br>
+    <a href = "lr6/index.php">ЛР6</a><br>
+    <a href = "lr7/index.php">ЛР7</a><br>
+    <a href = "lr8/index.php">ЛР8</a><br>
+    <a href = "lr9/index.php">ЛР9</a><br>
+</body>
+</html>
